5639c570ea48082f7f6a9974625b94c1937fb931
Yazir <yazirzuniga29@gmail.com>
2024-06-17 08:17:11 -0600

    cm

---
+++ examen_final/src/main/java/com/mycompany/examen_final/Examen_final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_final;
+
+/**
+ *
+ * @author Yazir Zuñiga
+ */
+public class Examen_final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
